f5bfecaab5b78c057bd7469759373ffdbd5b7c4b
Prakrypt <prakrypt@gmail.com>
2026-07-12 21:19:56 +0530

    Time: 3 ms (43.85%), Space: 8.3 MB (14.1%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans = {};
+        for(int i = 0;i<command.size();i++)
+        {
+            if(command[i]=='G') ans+='G';
+            if(command[i]==')')
+            {
+                if(command[i-1] == 'l') ans+="al";
+                else ans+='o';
+            }
+        } 
+        return ans;
+    }
+};
