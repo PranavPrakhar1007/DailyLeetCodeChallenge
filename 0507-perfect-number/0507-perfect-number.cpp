fb232b0a716a929cdd74e22ed3569bebe4be13a2
Prakrypt <prakrypt@gmail.com>
2026-07-17 14:14:07 +0530

    Time: 0 ms (100%), Space: 7.7 MB (90.26%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int count = 0;
+        if(num==1) return false;
+        for(int i=2;i*i<=num;i++)
+        {
+            if(num%i==0)
+            {
+                count +=i;
+                if(i*i != num)
+                {
+                    count+= num/i;
+                }
+            }
+            
+        }
+        count++;
+        return count == num;
+    }
+};
